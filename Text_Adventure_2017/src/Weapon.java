66eeecc16320202424aeb3b394c0c5be0c6bcc0f
Jared McInerney <JJMC106@gmail.com>
2017-11-09 13:09:06 +0000

    Moved around Classes. Created a folder for classes that are now redundant
    Weapon revised -> weapon
    Weapon -> ClassesNotNeeded/WeaponNO_USE

--- Text_Adventure_2017/src/Weapon.java
+++ Text_Adventure_2017/src/Weapon.java
@@ -1,26 +1,25 @@
-public class Weapon {
-
-    private double Cut_Damage;
-    private double Bludegeon_Damage;
-    private double Piercing_Damage;
+public class Weapon{
+    private double Damage;
+    private String Damage_Type;
     private double weight;
 
     //Mutators
-    public void setCut_Damage(double cut_Damage) { Cut_Damage = cut_Damage; }
-    public void setBludegeon_Damage(double bludegeon_Damage) { Bludegeon_Damage = bludegeon_Damage; }
-    public void setPiercing_Damage(double piercing_Damage) { Piercing_Damage = piercing_Damage; }
+    public void setDamage(double Damage) { Damage = Damage; }
+    public void setDamage_Type(String damage_Type) { Damage_Type = damage_Type; }
     public void setWeight(double weight) { this.weight = weight; }
 
     //Accessors
-    public double getCut_Damage() { return Cut_Damage; }
-    public double getBludegeon_Damage() { return Bludegeon_Damage; }
-    public double getPiercing_Damage() { return Piercing_Damage; }
+    public double getDamage() { return Damage; }
+    public String getDamage_Type() { return Damage_Type; }
     public double getWeight() { return weight; }
 
+
     public String toString() {
-        return String.format("%-20s\n%-5\n%-20s\n%-5\n%-20s\n%-5\n%-20s\n%-5\n%",
-                "Cut Damage: " + getCut_Damage() + "Bludegeon Damage: " +
-                getBludegeon_Damage() + "Piercing Damage: " + getPiercing_Damage() +
-                "Weight: " + getWeight());
+        return String.format("%-20s\n%-.1f\n%-20s\n%-12s\n%-20s\n%.1f",
+                "Damage: " + getDamage() + "Damage Type: " +
+                        getDamage_Type() + "Weight: " + getWeight());
+    }
+    public Weapon(){
+
     }
 }
